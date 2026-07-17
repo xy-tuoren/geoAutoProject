f87df949fa8afdd2a7ff5f1ac4d189800eb6d90a
xyTuoren <2895845213@qq.com>
2026-07-17 09:48:00 +0800

    切换 Windows 更新发布服务器

--- tests/node/update-feed-config.test.js
+++ tests/node/update-feed-config.test.js
@@ -6,6 +6,6 @@ test('Windows 安装包使用服务器 generic 更新源', () => {
   const manifest = require(path.join(__dirname, '..', '..', 'package.json'))
   assert.deepEqual(manifest.build.publish, [{
     provider: 'generic',
-    url: 'http://43.138.254.94/geo-updates/win/',
+    url: 'http://104.168.30.172/geo-updates/win/',
   }])
 })
